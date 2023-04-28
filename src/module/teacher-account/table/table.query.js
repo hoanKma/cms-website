2f59da09a7d26c01b40a75f207c28469bed1a6df
Lưu Ngọc Hoan <Hoan.kma2x@gmail.com>
2023-04-28 09:25:05 +0700

    sửa bảng tài khoản giáo viên

--- src/module/teacher-account/table/table.query.js
+++ src/module/teacher-account/table/table.query.js
@@ -1,10 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
+import { useLocation } from 'react-router-dom';
 import API from 'util/api';
 
 export const useQueryTableDataTeacherAccount = () => {
-  return useQuery(['GET_TABLE_TEACHER_ACCOUNT'], () => {
+  const location = useLocation();
+  const { search } = location;
+  return useQuery(['GET_TABLE_TEACHER_ACCOUNT', search], () => {
     return API.request({
-      url: '/users',
+      url: `/users${search || '?page=1'}&size=10`,
+
       params: {
         role: 'TEACHER'
       }
