647797924d26d0bb2febe66cc2d5f3ff044441dd
Lưu Ngọc Hoan <Hoan.kma2x@gmail.com>
2023-05-07 22:49:27 +0700

    thêm filter trong màn câu hỏi

--- src/module/blog/table/filter/filter.js
+++ src/module/blog/table/filter/filter.js
@@ -1,19 +1,24 @@
+import { Flex } from '@chakra-ui/react';
 import DropDownlist from 'base-component/drop-downlist';
-import { memo, useCallback, useEffect, useRef } from 'react';
+import { useQueryTopicBySubject } from 'module/blog/create/query';
+import { memo, useCallback, useEffect, useRef, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { subjectAtom } from 'state-management/subject';
+import { QUESTION_LEVEL } from 'util/const';
 import { paramsToObject } from 'util/helper';
 
 const Filter = () => {
   const firstTime = useRef(false);
-
+  const [currentSubjectCreate, setCurrentSubjectCreate] = useState();
   const dropDownListRef = useRef();
 
   const subjectData = useRecoilValue(subjectAtom);
 
   const searchParams = useSearchParams();
   const setSearchParams = searchParams[1];
+
+  const { data: topicData } = useQueryTopicBySubject(currentSubjectCreate);
 
   useEffect(() => {
     if (!firstTime.current) {
@@ -25,24 +30,68 @@ const Filter = () => {
     }
   }, [firstTime, setSearchParams, subjectData]);
 
-  const onChange = useCallback(
+  const onChangeSubject = useCallback(
     ({ e }) => {
+      setCurrentSubjectCreate(e?.id);
       setSearchParams({ subjectId: e?.id });
     },
     [setSearchParams]
   );
 
+  const onChangeTopic = useCallback(
+    ({ e }) => {
+      setSearchParams((curr) => {
+        const clone = paramsToObject(curr.entries());
+        return new URLSearchParams({ ...clone, topicId: e?.id });
+      });
+    },
+    [setSearchParams]
+  );
+
+  const onChangeLevel = useCallback(
+    ({ e }) => {
+      setSearchParams((curr) => {
+        const clone = paramsToObject(curr.entries());
+        return new URLSearchParams({ ...clone, level: e?.value });
+      });
+    },
+    [setSearchParams]
+  );
+
   return (
-    <DropDownlist
-      ref={dropDownListRef}
-      isMulti={false}
-      onChange={onChange}
-      isClearable={false}
-      isSearchable={false}
-      isRequired
-      options={subjectData}
-      defaultValue={subjectData[0]}
-    />
+    <Flex w={'fit-content'} gap={4}>
+      <DropDownlist
+        ref={dropDownListRef}
+        isMulti={false}
+        onChange={onChangeSubject}
+        isClearable={false}
+        isSearchable={false}
+        isRequired
+        options={subjectData}
+      />
+      <DropDownlist
+        name={'topic'}
+        isMulti={false}
+        placeholder="Chọn Chuyên đề"
+        onChange={onChangeTopic}
+        isClearable={false}
+        isSearchable={false}
+        isRequired
+        options={topicData}
+        labelKey="title"
+        valueKey="id"
+      />
+
+      <DropDownlist
+        name="level"
+        placeholder="Chọn Cấp độ"
+        isClearable={false}
+        isSearchable={false}
+        isRequired={true}
+        onChange={onChangeLevel}
+        options={QUESTION_LEVEL}
+      />
+    </Flex>
   );
 };
 
